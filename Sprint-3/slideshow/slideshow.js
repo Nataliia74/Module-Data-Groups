48c442fc0359afa65665eeaab8d25f5e0b180b7f
Nataliia74 <volkov.nataliia@gmail.com>
2025-07-25 15:36:37 +0100

    set interval for autoslidshow and according buttons

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -18,6 +18,18 @@ const slide = document.getElementById("carousel-img");
 const forwardButton = document.getElementById("forward-btn");
 const backwardButton = document.getElementById("backward-btn");
 
+const autoForwardButton = document.createElement("button");
+autoForwardButton.id = "auto-forward";
+autoForwardButton.innerText = "Auto Forward";
+
+const autoBackwardButton = document.createElement("button");
+autoBackwardButton.id = "auto-backward";
+autoBackwardButton.innerText = "Auto Backward";
+
+const stopButton = document.createElement("button");
+stopButton.id = "stop";
+stopButton.innerText = "Stop";
+
 const screen = document.createElement("div");
 screen.id = "screen";
 document.body.appendChild(screen);
@@ -26,9 +38,16 @@ screen.appendChild(slide);
 const buttons = document.createElement("div");
 buttons.id = "buttons";
 document.body.appendChild(buttons);
-buttons.append(forwardButton, backwardButton);
+buttons.append(
+  autoBackwardButton,
+  backwardButton,
+  stopButton,
+  forwardButton,
+  autoForwardButton
+);
 
 let startIndex = 0;
+let timer = 0;
 
 forwardButton.addEventListener("click", () => {
   startIndex = (startIndex + 1) % images.length;
@@ -38,4 +57,29 @@ forwardButton.addEventListener("click", () => {
 backwardButton.addEventListener("click", () => {
   startIndex = (startIndex - 1 + images.length) % images.length;
   slide.src = images[startIndex];
+});
+
+function slideShow() {
+  clearInterval(timer);
+  timer = setInterval(() => {
+    startIndex = (startIndex + 1) % images.length;
+    slide.src = images[startIndex];
+  }, 5000);
+}
+
+autoForwardButton.addEventListener("click", () => {
+  clearInterval(timer);
+  slideShow();
+});
+
+autoBackwardButton.addEventListener("click", () => {
+  clearInterval(timer);
+  timer = setInterval(() => {
+    startIndex = (startIndex - 1 + images.length) % images.length;
+    slide.src = images[startIndex];
+  }, 5000);
+});
+
+stopButton.addEventListener("click", () => {
+  clearInterval(timer);
 });
