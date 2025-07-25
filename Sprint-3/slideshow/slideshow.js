217cc1b8f659652ec145c1581aa6c8c42218617c
Nataliia74 <volkov.nataliia@gmail.com>
2025-07-25 13:27:08 +0100

    move images by using forward and backward buttons

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,8 +1,31 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
 ];
 
+const imagesArray = document.querySelectorAll("img");
+imagesArray.forEach((image) => {
+  image.width = 600;
+  image.height = 450;
+});
+// Write your code here#
+// const screen = document.createElement("div");
+// screen.id = "screen";
+// document.body.appendChild(screen);
 
-// Write your code here
+const slide = document.getElementById("carousel-img");
+const forwardButton = document.getElementById("forward-btn");
+const backwardButton = document.getElementById("backward-btn");
+
+let startIndex = 0;
+
+forwardButton.addEventListener("click", () => {
+  startIndex = (startIndex + 1) % images.length;
+  slide.src = images[startIndex];
+});
+
+backwardButton.addEventListener("click", () => {
+  startIndex = (startIndex - 1 + images.length) % images.length;
+  slide.src = images[startIndex];
+});
